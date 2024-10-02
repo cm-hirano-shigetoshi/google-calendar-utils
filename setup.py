0e864940a334dbf16efdf0820db9a8fa90f123cb
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2024-10-02 22:50:41 +0900

    依存パッケージ

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="google_calendar_utils",
     version="0.1",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=["google_auth_oauthlib", "google-api-python-client"],
     url="https://github.com/cm-hirano-shigetoshi/google-calendar-utils",
     author="cm-hirano.shigetoshi",
     author_email="hirano.shigetoshi@classmethod.jp",
